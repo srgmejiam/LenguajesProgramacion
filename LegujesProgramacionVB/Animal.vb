6100e694a01730fbcad22e9d5f6bbe6a274628c7
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-02-10 13:03:24 -0600

    agregando proyecto visual basic

---
+++ LegujesProgramacionVB/Animal.vb
@@ -0,0 +1,29 @@
+﻿Public MustInherit Class Animal
+    Public Property Nombre As String
+    Public MustOverride Sub HacerRuido()
+End Class
+
+Public Class Perro
+    Inherits Animal
+
+    Public Overrides Sub HacerRuido()
+        Console.WriteLine("Guau guau")
+    End Sub
+End Class
+
+Public Class Gato
+    Inherits Animal
+
+    Public Overrides Sub HacerRuido()
+        Console.WriteLine("Miau miau")
+    End Sub
+End Class
+
+Public Class Loro
+    Inherits Animal
+
+    Public Overrides Sub HacerRuido()
+        Console.WriteLine("Loro loro")
+    End Sub
+End Class
+
---
+++ LegujesProgramacionVB/Principal.vb
@@ -0,0 +1,16 @@
+﻿Module Principal
+    Sub Main()
+        Dim animales As List(Of Animal) = New List(Of Animal) From {
+            New Perro With {.Nombre = "Fido"},
+            New Gato With {.Nombre = "Mimi"},
+            New Loro With {.Nombre = "Piolín"}
+        }
+
+        For Each animal In animales
+            Console.WriteLine("El animal " & animal.Nombre & " hace:")
+            animal.HacerRuido()
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
